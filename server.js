e8b222e76fcdb2ff884b9c5d042111233b2abc4c
matheuspf <matheuspff@gmail.com>
2018-06-19 17:34:47 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express")
+var path = require("path");
+
+var app = express();
+
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+app.use("/lib", express.static(__dirname + "/lib"));
+app.use("/plugin", express.static(__dirname + "/plugin"));
+
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/index.html"))
+});
+
+app.listen(80);
